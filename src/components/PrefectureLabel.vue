e98fd1d5e5852f52baacdc37f9cb39f55fe5512d
kouta0530 <16jk047@ymklab.net>
2021-03-03 04:17:11 +0900

    feat(prefecture) prefectureLabelコンポーネントの追加

---
+++ src/components/PrefectureLabel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p
+      class="label"
+      :style="{ 'font-size': size + 'px', color: color, 'text-align': align }"
+    >
+      {{ label }}
+    </p>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class PrefectureLabel extends Vue {
+  @Prop()
+  label?: string;
+
+  @Prop({ default: 16 })
+  size?: number;
+
+  @Prop({ default: "black" })
+  color?: string;
+
+  @Prop()
+  align?: string;
+}
+</script>
+<style></style>
